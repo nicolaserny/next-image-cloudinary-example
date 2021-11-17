39ad5a45a909605fe9b9fe0ea3a52cbc05141c0c
nicolaserny <nicolas.erny@xcomponent.com>
2021-11-17 18:20:29 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const cloudinaryBaseUrl = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/`;
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    CLOUDINARY_BASE_URL: cloudinaryBaseUrl,
+  },
+  images: {
+    loader: "cloudinary",
+    path: cloudinaryBaseUrl,
+  },
+};
