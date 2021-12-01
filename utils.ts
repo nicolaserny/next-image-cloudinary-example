2564987c7da499e4aff3e4715cd942d70e13dbc3
nicolaserny <nicolas.erny@xcomponent.com>
2021-12-01 16:15:36 +0100

    Fix utils

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 export async function getBase64ImageUrl(imageId: string): Promise<string | undefined> {
-    const response = await fetch(`${process.env.CLOUDINARY_BASE_URL}w_100/e_blur:1000,q_auto,f_auto${imageId}`);
+    const response = await fetch(`${process.env.CLOUDINARY_BASE_URL}w_100/e_blur:1000,q_auto,f_webp${imageId}`);
     const buffer = await response.arrayBuffer();
     const data = Buffer.from(buffer).toString('base64');
-    return `data:image/jpeg;base64,${data}`;
+    return `data:image/webp;base64,${data}`;
 }
